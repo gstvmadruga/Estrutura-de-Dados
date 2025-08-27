5a57e4176600140df4eaa042cda0278d1207c7f2
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-08-26 21:32:04 -0300

    Update programa01.java

--- ProgramaVector/controle/programa01.java
+++ ProgramaVector/controle/programa01.java
@@ -1,1 +1,20 @@
+package controle;
+import java.util.Vector;
+
+public class Programa01 {
+
+	public static void main(String[] args) {
+		Vector colecao = new Vector();
+		
+		String nome = "Gariel";
+		Integer idade = 23;
+		Float altura = 1.73f;
+		
+		colecao.add(nome);
+		colecao.add(idade);
+		colecao.add(altura);
+		
+		System.out.print(colecao);
+	}	
+}
 
