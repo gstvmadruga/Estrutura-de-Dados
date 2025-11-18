d8f6f90015344f558628b0da2497861d44c11b43
Gustavo de Madruga Maciel <126895105+gstvmadruga@users.noreply.github.com>
2025-11-18 20:33:11 -0300

    Update Aluno.java

--- Trabalho/dominio/Aluno.java
+++ Trabalho/dominio/Aluno.java
@@ -1,1 +1,50 @@
+package dominio;
+
+public class Aluno {
+
+	    private String nome;
+	    private String matricula;
+	    private Professor professor; 
+
+	    public Aluno(String nome, String matricula) {
+	        this.nome = nome;
+	        this.matricula = matricula;
+	        this.professor = null;
+	    }
+
+	    public String getNome() {
+	        return nome;
+	    }
+
+	    public void setNome(String nome) {
+	        this.nome = nome;
+	    }
+
+	    public String getMatricula() {
+	        return matricula;
+	    }
+
+	    public void setMatricula(String matricula) {
+	        this.matricula = matricula;
+	    }
+
+	    public Professor getProfessor() {
+	        return professor;
+	    }
+
+	    public void setProfessor(Professor professor) {
+	        this.professor = professor;
+	    }
+
+	    @Override
+	    public String toString() {
+	        if (professor != null) {
+	            return nome + " (" + matricula + ") - Professor: " + professor.getNome();
+	        } else {
+	            return nome + " (" + matricula + ") - Sem professor";
+	        }
+	    }
+	}
+
+
 
